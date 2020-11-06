efbb432c15124ddef7685d6d594588a8c25a084a
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-06 19:57:56 +0900

    201106 | Update Interactive - circle polygon

--- Interactive/rotating_polygon/polygon.js
+++ Interactive/rotating_polygon/polygon.js
@@ -15,6 +15,7 @@ export class Polygon {
     // ctx.beginPath();
 
     const angle = PI2 / this.sides;
+    const angle2 = PI2 / 4;
 
     ctx.translate(this.x, this.y);
 
@@ -25,11 +26,18 @@ export class Polygon {
       const x = this.radius * Math.cos(angle * i);
       const y = this.radius * Math.sin(angle * i);
 
-      //   i == 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
-
+      ctx.save();
+      ctx.translate(x, y);
+      ctx.rotate((((360 / this.sides) * i + 45) * Math.PI) / 180);
       ctx.beginPath();
-      ctx.arc(x, y, 30, 0, PI2, false);
+      for (let j = 0; j < 4; j++) {
+        const x2 = 80 * Math.cos(angle2 * j);
+        const y2 = 80 * Math.sin(angle2 * j);
+        j == 0 ? ctx.moveTo(x2, y2) : ctx.lineTo(x2, y2);
+      }
       ctx.fill();
+      ctx.closePath();
+      ctx.restore();
     }
 
     // ctx.fill();
