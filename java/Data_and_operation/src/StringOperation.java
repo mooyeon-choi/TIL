2ee95304f225cde142afa5c85a3a8633bd78aeba
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-12 21:22:19 +0900

    201112 | Add java - Number, StringApp, StringOperation

---
+++ java/Data_and_operation/src/StringOperation.java
@@ -0,0 +1,9 @@
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, [[[name]]] ... bye".replace("[[[name]]]", "moomoo"));
+	}
+
+}
