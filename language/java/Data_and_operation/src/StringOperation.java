94214bdb5a2ce4041950c8d5d242a28aa9815629
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-25 16:03:52 +0900

    Restart java study | language - java

--- language/java/Data_and_operation/src/StringOperation.java
+++
@@ -1,9 +0,0 @@
-public class StringOperation {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Hello World".length()); // 11
-		System.out.println("Hello, [[[name]]] ... bye".replace("[[[name]]]", "moomoo"));
-	}
-
-}
--- language/java/Programming/src/org/opentutorials/iot/OnOff.java
+++
@@ -1,6 +0,0 @@
-package org.opentutorials.iot;
-
-public interface OnOff {
-	public boolean on();
-	public boolean off();
-}
