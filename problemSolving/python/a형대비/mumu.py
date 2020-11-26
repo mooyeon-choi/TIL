9d9bed08bbb70d5998af4325e05b85859f3bc0ba
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-26 23:37:21 +0900

    Add PS and replace Algorithm file | PS

--- problemSolving/python/a형대비/mumu.py
+++
@@ -1,4 +0,0 @@
-mumus = [['☆mumu★'] * 100 for _ in range(100)]
-mumus[95][5] = '☆amumu★'
-for mumu in mumus:
-    print(*mumu)
--- problemSolving/python/naverhack/pro.4.py
+++
@@ -1,7 +0,0 @@
-def Solution(A, B):
-    answer = 0
-    num = A * B
-    while num != 0:
-        answer += num%2
-        num //= 2
-    return answer
