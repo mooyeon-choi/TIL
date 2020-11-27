2cac99eea577d6c6c2080a50173b6c341f1c1cae
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-27 09:51:46 +0900

    Add reset button in habit-tracker | web FE react

--- web/frontend/react/react_basic/habit-tracker/src/app.jsx
+++ web/frontend/react/react_basic/habit-tracker/src/app.jsx
@@ -37,6 +37,14 @@ class App extends Component {
     this.setState({ habits });
   }
 
+  handleReset = () => {
+    const habits = this.state.habits.map(habit => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  }
+
   render() {
     return (
       <>
@@ -47,6 +55,7 @@ class App extends Component {
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
           onAdd={this.handleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
--- web/frontend/react/react_basic/habit-tracker/src/components/habits.jsx
+++ web/frontend/react/react_basic/habit-tracker/src/components/habits.jsx
@@ -42,6 +42,9 @@ class Habits extends Component {
           />
         )}
       </ul>;
+      <button className="habits-reset" onClick={this.props.onReset}>
+        Reset All
+      </button>
       </>
     )
   }
