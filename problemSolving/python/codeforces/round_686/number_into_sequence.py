29e5952f15f1b46aaf2384d388f75c9791f86340
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-29 10:02:29 +0900

    Add Number Into Sequence(Unsolved) | PS python codeforces

--- problemSolving/python/codeforces/round_686/number_into_sequence.py
+++ problemSolving/python/codeforces/round_686/number_into_sequence.py
@@ -1,16 +1,33 @@
-def dfs(num, cnt, n):
-    for i in range(max(n, 2), num // 2, n):
+def dfs(num, cnt, n_list):
+    for i in range(2, int((num)**(1/2)) + 1):
         if not num % i:
-            if not i % n:
+            for n in n_list:
+                if i % n:
+                    break
+            else:
                 nums.append(i)
-                return dfs(num//i, cnt + 1, n * i)
+                cnt = dfs(num//i, cnt + 1, n_list + [i])
+                if cnt > 1:
+                    return cnt
+                nums.pop()
     else:
-        nums.append(num)
-        return cnt
+        if cnt > 1:
+            for n in n_list:
+                if num % n:
+                    return 1
+            else:
+                nums.append(num)
+                return cnt
+        else:
+            return cnt
 
 t = int(input())
 for _ in range(t):
     num = int(input())
     nums = []
-    print(dfs(num, 1, 1))
-    print(' '.join(map(str, nums)))
+    result = dfs(num, 1, [1])
+    print(result)
+    if result > 1:
+        print(' '.join(map(str, nums)))
+    else:
+        print(num)
