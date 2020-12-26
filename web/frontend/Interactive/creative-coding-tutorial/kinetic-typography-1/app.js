b63cef9b14025a90ec8857d9006bbbf7bb0aa60d
mooyeon-choi <dus1208@ajou.ac.kr>
2020-12-26 19:01:16 +0900

    Update particle using PIXI | Web FE Interactive

--- web/frontend/Interactive/creative-coding-tutorial/kinetic-typography-1/app.js
+++ web/frontend/Interactive/creative-coding-tutorial/kinetic-typography-1/app.js
@@ -1,21 +1,89 @@
-import { Text } from "./text.js";
+import { Visual } from "./visual.js";
 
 class App {
   constructor() {
+    this.setWebgl();
+
     WebFont.load({
       google: {
         families: ["Hind:700"],
       },
       fontactive: () => {
-        this.text = new Text();
-        this.text.setText(
-          "A",
-          2,
-          document.body.clientWidth,
-          document.body.clientHeight
-        );
+        this.visual = new Visual();
+
+        window.addEventListener("resize", this.resize.bind(this), false);
+        this.resize();
+
+        requestAnimationFrame(this.animate.bind(this));
       },
     });
+  }
+
+  setWebgl() {
+    this.renderer = new PIXI.Renderer({
+      width: document.body.clientWidth,
+      height: document.body.clientHeight,
+      antialias: true,
+      transparent: false,
+      resolution: window.devicePixelRatio > 1 ? 2 : 1,
+      autoDensity: true,
+      powerPreference: "high-performance",
+      backgroundColor: 0x2b27f5,
+    });
+    document.body.appendChild(this.renderer.view);
+
+    this.stage = new PIXI.Container();
+
+    const blurFilter = new PIXI.filters.BlurFilter();
+    blurFilter.blur = 10;
+    blurFilter.autoFit = true;
+
+    const fragSource = `
+      precision mediump float;
+      varying vec2 vTextureCoord;
+      uniform sampler2D uSampler;
+      uniform float threshold;
+      uniform float mr;
+      uniform float mg;
+      uniform float mb;
+      void main(void) {
+        vec4 color = texture2D(uSampler, vTextureCoord);
+        vec3 mcolor = vec3(mr, mg, mb);
+        if (color.a > threshold) {
+          gl_FragColor = vec4(mcolor, 1.0);
+        } else {
+          gl_FragColor = vec4(vec3(0.0), 0.0);
+        }
+      }
+    `;
+
+    const uniformsData = {
+      threshold: 0.5,
+      mr: 68.0 / 255.0,
+      mg: 224.0 / 255.0,
+      mb: 255.0 / 255.0,
+    };
+
+    const thresholdFilter = new PIXI.Filter(null, fragSource, uniformsData);
+    this.stage.filters = [blurFilter, thresholdFilter];
+    this.stage.filterArea = this.renderer.screen;
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.renderer.resize(this.stageWidth, this.stageHeight);
+
+    this.visual.show(this.stageWidth, this.stageHeight, this.stage);
+  }
+
+  animate(t) {
+    requestAnimationFrame(this.animate.bind(this));
+
+    this.visual.animate();
+
+    this.renderer.render(this.stage);
   }
 }
 
--- web/frontend/Interactive/creative-coding-tutorial/kinetic-typography-1/text.js
+++ web/frontend/Interactive/creative-coding-tutorial/kinetic-typography-1/text.js
@@ -1,10 +1,10 @@
 export class Text {
   constructor() {
     this.canvas = document.createElement("canvas");
-    this.canvas.style.position = "absolute";
-    this.canvas.style.left = "0";
-    this.canvas.style.top = "0";
-    document.body.appendChild(this.canvas);
+    //this.canvas.style.position = "absolute";
+    //this.canvas.style.left = "0";
+    //this.canvas.style.top = "0";
+    //document.body.appendChild(this.canvas);
 
     this.ctx = this.canvas.getContext("2d");
   }
