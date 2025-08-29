9952aa1f636bd852843e45a0532df2f558c69a6b
mooyeon-choi <lon12080@gmail.com>
2025-08-29 19:06:23 +0900

    add: implement greedy solution for 닭과 소 (14464) and optimize 공항 (10775) solution

--- problemSolving/baekjoon/step/greedy/공항_10775.js
+++ problemSolving/baekjoon/step/greedy/공항_10775.js
@@ -11,15 +11,21 @@ const gates = input.slice(2);
 
 function solution() {
   let answer = 0;
-  const memo = new Set(Array(G + 1).fill(0).map((_, i) => i));
+  const next = Array.from({ length: G + 1 }, (_, i) => i);
+
+  function getNext(x) {
+    while (x > 0 && next[x] !== x) {
+      next[x] = next[next[x]];
+      x = next[x];
+    }
+    return x;
+  }
 
   for (let i = 0; i < P; i++) {
-    let num = gates[i];
-    while (!memo.has(num) && num > 0) {
-      num--;
-    }
-    if (num === 0) break;
-    memo.delete(num);
+    const g = gates[i];
+    let pos = getNext(g);
+    if (pos === 0) break;
+    next[pos] = pos - 1;
     answer++;
   }
 
